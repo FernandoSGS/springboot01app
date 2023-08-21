9e83a21b30846a6649beddaefb14d959799453a4
Fernando <nando.sgs.44@gmail.com>
2023-08-21 20:13:48 -0300

    Criando projeto springboot01app

---
+++ src/main/java/br/gov/sp/fatec/springboot01app/Springboot01appApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot01app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springboot01app/Springboot01appApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot01app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
