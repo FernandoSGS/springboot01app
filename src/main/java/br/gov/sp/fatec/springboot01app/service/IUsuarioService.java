f43567d7534047a561dc883176c9e3605e13236b
FernandoSGS <nando.sgs.44@gmail.com>
2023-08-28 21:35:05 +0000

    IUsuarioService.java updated

--- src/main/java/br/gov/sp/fatec/springboot01app/service/IUsuarioService.java
+++ src/main/java/br/gov/sp/fatec/springboot01app/service/IUsuarioService.java
@@ -2,6 +2,8 @@ package br.gov.sp.fatec.springboot01app.service;
 
 import java.util.List;
 
+import br.gov.sp.fatec.springboot01app.entity.Anotacao;
+import br.gov.sp.fatec.springboot01app.entity.Autorizacao;
 import br.gov.sp.fatec.springboot01app.entity.Usuario;
 
 public interface IUsuarioService {
@@ -10,6 +12,12 @@ public interface IUsuarioService {
 
     public List<Usuario> buscarTodosUsuarios();
 
-    public Usuario buscarPorId(Long id);
+    public Usuario buscarUsuarioPorId(Long id);
+
+    public void associaAutorizacao(Long idUsuario, Long idAutorizacao);
+    
+    public Autorizacao novaAutorizacao(Autorizacao autorizacao);
+
+    public Anotacao novaAnotacao(Anotacao anotacao);
     
 }
